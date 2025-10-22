abaebb28f12aa5658000e7ca056c3f3f9d839ae5
osh418 <brown8167@gmail.com>
2025-10-22 19:12:44 +0900

    feat: ESLint 설정 및 새로운 플러그인 추가
    - Updated eslint.config.js to include "@feature-sliced" configuration
    - Updated package.json to add "@feature-sliced/eslint-config" and new ESLint plugins: "eslint-plugin-boundaries" and "eslint-plugin-import"
    - Updated pnpm-lock.yaml to reflect the new dependencies and their versions

--- eslint.config.js
+++ eslint.config.js
@@ -14,6 +14,7 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      "@feature-sliced"
     ],
     languageOptions: {
       ecmaVersion: 2020,
