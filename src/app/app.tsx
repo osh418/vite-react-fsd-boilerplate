14047719997400103d3af29daf2f05748be9bf17
osh418 <brown8167@gmail.com>
2025-10-22 18:40:14 +0900

    feat: fsd에 맞게 구조 변경

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button } from "@/components/ui/button";
+import { Button } from "@/shared/ui/button";
 
 function App() {
   const [count, setCount] = useState(0);
